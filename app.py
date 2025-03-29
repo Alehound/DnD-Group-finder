8975f6197f509b8e55e4e36e0dab99f69cfe632c
Bryn Sloan <brynsloa@cubbli22-gold.local>
2025-03-29 19:11:43 +0200

    Add items module

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import redirect, render_template, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
 import db
 import config
+import items
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
@@ -22,8 +23,7 @@ def create_item():
     description = request.form["description"]
     user_id = session["user_id"]
 
-    sql = "INSERT INTO items (title, description, user_id) VALUES (?, ?, ?)"    
-    db.execute(sql, [title, description, user_id])
+    items.add_item(title, description, user_id)
 
     return redirect("/")
 
@@ -51,7 +51,7 @@ def create_user():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template("login.html")  
+        return render_template("login.html")
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
